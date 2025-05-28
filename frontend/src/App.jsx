6d825eca8431ba8c177f27e7381efac4bad574a9
Dev-Tanaay <kingspyder03@gmail.com>
2025-05-29 00:27:42 +0530

    Installation of the dependinces...

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='flex justify-center items-center min-h-screen '>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
